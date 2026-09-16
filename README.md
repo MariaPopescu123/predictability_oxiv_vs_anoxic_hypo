07f7861438d3d905cc8f96a887cee259fc643a5b
MariaPopescu <mariapopescu100@gmail.com>
2026-09-16 12:18:43 -0400

    added chunk for variables of interest. hardcoded what was being called in from yaml file

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# predictability_oxiv_vs_anoxic_hypo
+
+## Structure
+
+- so far work in progress:
+currently working on adding baseline models.
+To run baseline models go to "/baseline_models"
+Functions required for these are in "/R"
+
+added:
+climatology - includes climatology for all variables Austin had before, new chunk
+at the bottom for just my variables of interest which include:
+'SRP_ugL_sample', 'NO3NO2_ugL_sample', 'NH4_ugL_sample', 'DOC_mgL_sample',
+'CH4_umolL_sample','CO2_umolL_sample') at depths 0.1m & 6m for BVR and
+0.1m & 9m for FCR.
+
+
+currently adding:
--- baseline_models/climatology.R
+++ baseline_models/climatology.R
@@ -13,16 +13,22 @@ source('R/convert2binary.R')
 #' set the random number for reproducible MCMC runs
 set.seed(329)
 
-config <- yaml::read_yaml("challenge_configuration.yaml")
+#config <- yaml::read_yaml("challenge_configuration.yaml") going to hard code the things that are called in from here: https://github.com/LTREB-reservoirs/vera4cast/blob/main/challenge_configuration.yaml
 team_name <- "climatology"
 
 # where are the relevant targets?
-targets_insitu <- readr::read_csv(paste0("https://", config$endpoint, "/", config$targets_bucket, "/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz"), guess_max = 10000)
-targets_met <- readr::read_csv(paste0("https://", config$endpoint, "/", config$targets_bucket, "/project_id=vera4cast/duration=P1D/daily-met-targets.csv.gz"), guess_max = 10000, show_col_types = FALSE)
-targets_tubr <- readr::read_csv(paste0("https://", config$endpoint, "/", config$targets_bucket, "/project_id=vera4cast/duration=P1D/daily-inflow-targets.csv.gz"), guess_max = 10000, show_col_types = FALSE)
+targets_insitu <- readr::read_csv(paste0("https://", "amnh1.osn.mghpcc.org", "/", "bio230121-bucket01/vera4cast/targets", "/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz"), guess_max = 10000)
+targets_met <- readr::read_csv(paste0("https://", "amnh1.osn.mghpcc.org", "/", "bio230121-bucket01/vera4cast/targets", "/project_id=vera4cast/duration=P1D/daily-met-targets.csv.gz"), guess_max = 10000, show_col_types = FALSE)
+targets_tubr <- readr::read_csv(paste0("https://", "amnh1.osn.mghpcc.org", "/", "bio230121-bucket01/vera4cast/targets", "/project_id=vera4cast/duration=P1D/daily-inflow-targets.csv.gz"), guess_max = 10000, show_col_types = FALSE)
+
+# Keep a pristine copy of the insitu targets. Later in the script targets_insitu gets
+# mutated (BVR chem depths are all recoded to 1.5 m), which destroys the real depth
+# structure -- the depth-resolved forecasts below must be built from this untouched copy.
+targets_insitu_raw <- targets_insitu
 
 # Get site information
-sites <- readr::read_csv(config$catalog_config$site_metadata_url, show_col_types = FALSE)
+sites <- readr::read_csv('https://raw.githubusercontent.com/LTREB-reservoirs/vera4cast/main/vera4cast_field_site_metadata.csv'
+                         , show_col_types = FALSE)
 site_names <- sites$site_id
 
 # Inflow variables
@@ -82,7 +88,7 @@ site_var_combinations_deeper_depth_bvr <- expand.grid(var = c('DO_mgL_mean',
                                                               'Temp_C_mean',
                                                               'CH4_umolL_sample'),
                                                       site = 'bvre',
-                                                      depth = 8)
+                                                      depth = 6)
 
 climatology_insitu_deeper_bvr <- purrr::pmap_dfr(site_var_combinations_deeper_depth_bvr,
                                                  .f = ~ generate_baseline_climatology(targets = targets_insitu,
@@ -92,7 +98,7 @@ climatology_insitu_deeper_bvr <- purrr::pmap_dfr(site_var_combinations_deeper_de
                                                                                       #depth = 'target',
                                                                                       ...))
 
-## GHG VARIABLES (TAKEN FROM DIFFERENT DEPTH)
+## GHG VARIABLES (TAKEN FROM DIFFERENT DEPTH)####
 site_var_combinations_ghg_insitu <- expand.grid(var = c('CH4_umolL_sample',
                                                         'CO2_umolL_sample'),
                                                 site = c('fcre',
@@ -156,30 +162,34 @@ climatology_insitu_chla_max <- purrr::pmap_dfr(site_var_combinations_chla_max,
                                                                                     forecast_date = Sys.Date(),
                                                                                     depth = 'target',
                                                                                     ...))
-## CHEM variables
-site_var_combinations_chem <- expand.grid(var = c('TN_ugL_sample',
-                                                  'TP_ugL_sample',
+## CHEM variables Maria commented out the variables that I am not looking at####
+site_var_combinations_chem <- expand.grid(var = c(
+                                                  # 'TN_ugL_sample',
+                                                  # 'TP_ugL_sample',
                                                   'SRP_ugL_sample',
                                                   'NO3NO2_ugL_sample',
                                                   'NH4_ugL_sample',
-                                                  'DOC_mgL_sample',
-                                                  'DRSI_mgL_sample',
+                                                  'DOC_mgL_sample'
+                                                  # ,
+                                                  #'DRSI_mgL_sample',
                                                   #'DIC_mgL_sample',
-                                                  'DC_mgL_sample',
-                                                  'DN_mgL_sample'),
+                                                  # 'DC_mgL_sample',
+                                                  # 'DN_mgL_sample'
+                                                  ),
                                           site = c('fcre',
                                                    'bvre'))
 
 targets_insitu <- targets_insitu |>
-  mutate(depth_m = ifelse(variable %in% c('TN_ugL_sample',
-                                          'TP_ugL_sample',
-                                          'SRP_ugL_sample',
+  mutate(depth_m = ifelse(variable %in% c('SRP_ugL_sample',
                                           'NO3NO2_ugL_sample',
                                           'NH4_ugL_sample',
-                                          'DOC_mgL_sample',
-                                          'DC_mgL_sample',
-                                          'DN_mgL_sample',
-                                          'DIC_mgL_sample') & site_id == 'bvre',
+                                          'DOC_mgL_sample'
+                                          # ,'TN_ugL_sample',
+                                          # 'TP_ugL_sample',
+                                          # 'DC_mgL_sample',
+                                          # 'DN_mgL_sample',
+                                          # 'DIC_mgL_sample'
+                                          ) & site_id == 'bvre',
                           1.5,
                           depth_m))
 
@@ -261,19 +271,64 @@ forecast_file <- paste0(paste("daily", file_date, team_name, sep = "-"), ".csv.g
 
 write_csv(combined_climatology, forecast_file)
 
-# combined_climatology %>%
-#   filter(family == 'normal') |>
-#   pivot_wider(names_from = parameter, values_from = prediction) |>
-#   ggplot(aes(x = datetime, y = mu, fill = as_factor(depth_m))) +
-#   geom_line() +
-#   geom_ribbon(aes(ymax = mu+sigma, ymin = mu-sigma), alpha = 0.3, fill = 'blue') +
-#   facet_grid(variable~site_id, scales = 'free')
+#Maria changing this so I can just view my variables of interest (interested_climatology)
+### VARIABLES OF INTEREST, FORECAST AT SPECIFIC DEPTHS ####
+# Rather than pulling these out of combined_climatology (where each variable only exists
+# at whichever single depth its block above happened to use), generate them explicitly at
+# the depths I care about: a surface and a hypolimnetic depth at each site,
+# BVR 0.1 / 6 m and FCR 0.1 / 9 m.
+print('Variables of interest, by depth')
 
-# combined_climatology %>%
-#   filter(family == 'bernoulli') |>
-#   ggplot(aes(x = datetime, y = prediction, colour = as_factor(depth_m))) +
-#   geom_line() +
-#   facet_grid(variable~site_id, scales = 'free')
+interested_vars <- c('SRP_ugL_sample',
+                     'NO3NO2_ugL_sample',
+                     'NH4_ugL_sample',
+                     'DOC_mgL_sample',
+                     'CH4_umolL_sample',
+                     'CO2_umolL_sample')
+
+# NOTE: BVR grab samples are taken at 0.1 / 3 / 6 / 9 m -- there is no chem/GHG data at
+# 1.5 m (that is the sensor depth), so 3 m is the option if a mid-depth is ever wanted.
+interested_site_depths <- dplyr::bind_rows(
+  tidyr::expand_grid(site = 'bvre', depth = c(0.1, 6)),
+  tidyr::expand_grid(site = 'fcre', depth = c(0.1, 9)))
+
+# one row per site/depth/variable -> one call per combination, so each depth gets its own
+# climatology (the function interpolates within site_id/variable, not within depth)
+site_var_depth_interested <- tidyr::expand_grid(var = interested_vars,
+                                                interested_site_depths)
+
+interested_climatology <- purrr::pmap_dfr(site_var_depth_interested,
+                                          .f = ~ generate_baseline_climatology(targets = targets_insitu_raw,
+                                                                               h = 35,
+                                                                               model_id = team_name,
+                                                                               forecast_date = Sys.Date(),
+                                                                               ...))
+
+# which site/depth/variable combinations actually produced a forecast?
+interested_climatology |>
+  dplyr::distinct(site_id, variable, depth_m) |>
+  dplyr::arrange(site_id, variable, depth_m) |>
+  print(n = Inf)
+
+###plot####
+interested_climatology %>%
+  filter(family == 'normal') |>
+  pivot_wider(names_from = parameter, values_from = prediction) |>
+  mutate(depth_m = as_factor(depth_m)) |>
+  ggplot(aes(x = datetime, y = mu, colour = depth_m, fill = depth_m, group = depth_m)) +
+  geom_ribbon(aes(ymax = mu+sigma, ymin = mu-sigma), alpha = 0.2, colour = NA) +
+  geom_line() +
+  facet_grid(variable~site_id, scales = 'free') +
+  labs(colour = 'Depth (m)', fill = 'Depth (m)') +
+  # shrink the variable strip labels on the right so the long names stay readable
+  theme(strip.text.y = element_text(size = 6),
+        strip.text.x = element_text(size = 9))
+
+combined_climatology %>%
+  filter(family == 'bernoulli') |>
+  ggplot(aes(x = datetime, y = prediction, colour = as_factor(depth_m))) +
+  geom_line() +
+  facet_grid(variable~site_id, scales = 'free')
 
 # vera4castHelpers::submit(forecast_file = forecast_file,
 #                          ask = FALSE,
